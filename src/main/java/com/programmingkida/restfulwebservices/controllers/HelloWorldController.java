2225abd8df9c0e7467c83a4273038181ae9a2678
raju-nichit <meetrajnichit@gail.com>
2018-02-08 09:47:33 +0530

    Query param API added in the hello wolrd controller

--- src/main/java/com/programmingkida/restfulwebservices/controllers/HelloWorldController.java
+++ src/main/java/com/programmingkida/restfulwebservices/controllers/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.programmingkida.restfulwebservices.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.programmingkida.restfulwebservices.beans.HelloWorldBean;
@@ -26,5 +27,11 @@ public class HelloWorldController {
 		public HelloWorldBean helloWorldPathVariable(@PathVariable String name){
 		  return new  HelloWorldBean(String.format("Hello world, %s",name));
 	  }
+	
+	
+	@GetMapping(path="/hello-world-query-param")
+	public HelloWorldBean helloWorldQueryParam(@RequestParam("name") String name){
+	  return new  HelloWorldBean(String.format("Hello world, %s",name));
+  }
 	
 }
