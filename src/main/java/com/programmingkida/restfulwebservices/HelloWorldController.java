12ac500b50e35d3c035fda340ab7dc37380feea9
raju-nichit <meetrajnichit@gail.com>
2018-02-08 09:05:45 +0530

    get api by using @GetMapping annotation

--- src/main/java/com/programmingkida/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/programmingkida/restfulwebservices/HelloWorldController.java
@@ -1,13 +1,12 @@
 package com.programmingkida.restfulwebservices;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 	
-	@RequestMapping(method=RequestMethod.GET,path="/hello-world")
+	@GetMapping(path="/hello-world")
 		public String helloWorld(){
 		  return "Hello World";
 	  }
