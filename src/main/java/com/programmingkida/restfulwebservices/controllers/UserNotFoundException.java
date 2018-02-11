8d2791b99683b41f7f2e89847289b5cb0f0af1c6
raju-nichit <meetrajnichit@gail.com>
2018-02-11 11:05:30 +0530

    User not found response status chaned

--- src/main/java/com/programmingkida/restfulwebservices/controllers/UserNotFoundException.java
+++ src/main/java/com/programmingkida/restfulwebservices/controllers/UserNotFoundException.java
@@ -1,5 +1,9 @@
 package com.programmingkida.restfulwebservices.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code=HttpStatus.NOT_FOUND)
 public class UserNotFoundException  extends RuntimeException{
 
 	/**
