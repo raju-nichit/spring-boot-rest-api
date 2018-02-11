6946f02a52b0ee8c718813ace581c4d07f05bca6
raju-nichit <meetrajnichit@gail.com>
2018-02-11 10:52:11 +0530

    user create API reponse status code changed

--- src/main/java/com/programmingkida/restfulwebservices/controllers/UserController.java
+++ src/main/java/com/programmingkida/restfulwebservices/controllers/UserController.java
@@ -1,13 +1,17 @@
 package com.programmingkida.restfulwebservices.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponents;
 
 import com.programmingkida.restfulwebservices.beans.User;
 import com.programmingkida.restfulwebservices.dao.UserDao;
@@ -32,8 +36,13 @@ public class UserController {
 	
 	
 	@PostMapping("/users")
-	public User createUser(@RequestBody User user){
+	public ResponseEntity<Object> createUser(@RequestBody User user){
 		User savedUser=userDao.save(user);
-		return savedUser;
+		
+	URI resourceURI = ServletUriComponentsBuilder
+		.fromCurrentRequest()
+		.path("/{id}")
+		.buildAndExpand(savedUser.getId()).toUri();
+		return ResponseEntity.created(resourceURI).build();
 	}
 }
