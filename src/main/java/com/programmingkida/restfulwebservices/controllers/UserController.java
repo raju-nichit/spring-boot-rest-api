1a763e24a096ff8a97f38df779e262416dd36ba3
raju-nichit <meetrajnichit@gail.com>
2018-02-11 11:04:01 +0530

    User not found exception added

--- src/main/java/com/programmingkida/restfulwebservices/controllers/UserController.java
+++ src/main/java/com/programmingkida/restfulwebservices/controllers/UserController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-import org.springframework.web.util.UriComponents;
 
 import com.programmingkida.restfulwebservices.beans.User;
 import com.programmingkida.restfulwebservices.dao.UserDao;
@@ -31,14 +30,18 @@ public class UserController {
 	
 	@GetMapping("/users/{id}")
 	public User retiveUser(@PathVariable Integer id){
-		return  userDao.findOne(id);
+		
+		 User user = userDao.findOne(id);
+		 if(user==null) {
+			 throw new UserNotFoundException("id-"+id);
+		 }
+		 return user;
 	}
 	
 	
 	@PostMapping("/users")
 	public ResponseEntity<Object> createUser(@RequestBody User user){
 		User savedUser=userDao.save(user);
-		
 	URI resourceURI = ServletUriComponentsBuilder
 		.fromCurrentRequest()
 		.path("/{id}")
